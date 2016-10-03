265649c5cbe7501658395a62f86445a8fc6920c3
jayakumar <jayakumar.lavudi@gmail.com>
2016-10-02 22:54:51 -0500

    Added CRUD project and documentation

---
+++ CRUD Operations/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using AutomatedTellerMachine.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET /home/index        
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(c => c.ApplicationUserId == userId).First().Id;
+            ViewBag.CheckingAccountId = checkingAccountId;
+            
+            var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = manager.FindById(userId);
+            ViewBag.Pin = user.Pin;
+            return View();
+        }
+
+        // GET /home/about        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+                
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble? Send us a message.";            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            // TODO: send the message to HQ
+            ViewBag.TheMessage = "Thanks, we got your message!";
+
+            return PartialView("_ContactThanks");
+        }
+
+        public ActionResult Foo()
+        {            
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASP.NET MVC5 ONLINE ATM";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD Operations/AutomatedTellerMachine/Models/IdentityModels.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace AutomatedTellerMachine.Models
+{
+   
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public string Pin { get; set; }
+    }    
+}
